2237584e934ac251f957e72d0366183c6d24173d
Cristobal <cristobal.navarro.g@gmail.com>
2018-10-10 17:47:43 -0300

    re organized folders

---
+++ matmuls/kernel.cuh
@@ -0,0 +1,162 @@
+#ifndef _KERNEL_H_
+#define _KERNEL_H_
+using namespace nvcuda;
+
+// The only dimensions currently supported by WMMA
+const int WMMA_M = 16;
+const int WMMA_N = 16;
+const int WMMA_K = 16;
+
+__global__ void convertFp32ToFp16 (half *out, float *in, int n) {
+   int idx = blockDim.x * blockIdx.x + threadIdx.x;
+   if (idx < n) {
+      out[idx] = in[idx];
+   }
+}
+
+// kernel
+__global__ void matmuls_basic(REAL* A, REAL* B, REAL *C, int n){
+    int off = blockIdx.x * (TCSIZE*TCSIZE);
+    int tid = off + (threadIdx.y*TCSIZE + threadIdx.x);
+    for(int i=0; i<TCSIZE; ++i){
+        C[tid] += A[off + threadIdx.y*TCSIZE + i]*B[off + i*TCSIZE + threadIdx.x];
+    }
+}
+
+__global__ void matmuls_sm(REAL* A, REAL* B, REAL *C, int n){
+    __shared__ REAL As[TCSIZE*TCSIZE];
+    __shared__ REAL Bs[TCSIZE*TCSIZE];
+    __shared__ REAL Cs[TCSIZE*TCSIZE];
+    int off = blockIdx.x * (TCSIZE*TCSIZE);
+    int ltid = threadIdx.y*TCSIZE + threadIdx.x;
+    // (1) cargar datos de global mem a cache
+    As[ltid] = A[off + ltid];
+    Bs[ltid] = B[off + ltid];
+    Cs[ltid] = 0.0f;
+    // (2) sync
+    __syncthreads();
+    // (3) hacer multiplicacion usando As, Bs, Cs 
+    #pragma unroll
+    for(int i=0; i<TCSIZE; ++i){
+        Cs[ltid] += As[threadIdx.y*TCSIZE + i]*Bs[i*TCSIZE + threadIdx.x];
+    }
+    // (4) copiar Cs a memoria global.
+    C[off + ltid] = Cs[ltid];
+}
+
+
+
+
+
+
+__global__ void matmuls_tc(half* A, half* B, REAL *C, int n){
+    int off = blockIdx.x * TCSQ;
+
+    // solo el primer warp trabajara, de ocho warps
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a_frag;
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b_frag;
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, REAL> c_frag;
+
+    // (3) cargar datos de memoria global a A, B y C frags
+    wmma::load_matrix_sync(a_frag, A + off, TCSIZE);
+    wmma::load_matrix_sync(b_frag, B + off, TCSIZE);
+    //wmma::fill_fragment(a_frag, (half)1.0f);
+    //wmma::fill_fragment(b_frag, (half)1.0f);
+    wmma::fill_fragment(c_frag, 0.0f);
+
+    // (4) operacion matrix-multiply-accumulate (MMA)
+    wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+
+    // (5) guardar datos de vuelta en memorial global
+    wmma::store_matrix_sync(C + off, c_frag, TCSIZE, wmma::mem_row_major);
+
+}
+
+__global__ void matmuls_tc_block(half* A, half* B, REAL *C, int n){
+    int off = blockIdx.x * TCSQ * 8;
+    int ltid = threadIdx.y*TCSIZE + threadIdx.x;
+    int woff = (ltid >> 5) << 8;
+    //int woff = (ltid/32) * 256;
+
+    // solo el primer warp trabajara, de ocho warps
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a_frag;
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b_frag;
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, REAL> c_frag;
+
+    // (3) cargar datos de memoria global a A, B y C frags
+    wmma::load_matrix_sync(a_frag, A + off + woff, TCSIZE);
+    wmma::load_matrix_sync(b_frag, B + off + woff, TCSIZE);
+    wmma::fill_fragment(c_frag, 0.0f);
+
+    // (4) operacion matrix-multiply-accumulate (MMA)
+    wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+
+    // (5) guardar datos de vuelta en memorial global
+    wmma::store_matrix_sync(C + off + woff, c_frag, TCSIZE, wmma::mem_row_major);
+}
+
+__global__ void matmuls_tc_block_sm(half* A, half* B, REAL *C, int n){
+    int off = blockIdx.x * TCSQ * 8;
+    __shared__ half As[8*TCSQ];
+    __shared__ half Bs[8*TCSQ];
+    int ltid = threadIdx.y*TCSIZE + threadIdx.x;
+    //int woff = (ltid >> 5)*TCSQ;
+    int woff = (ltid >> 5) << 8;
+    // (1) cargar datos de global mem a cache
+    #pragma unroll
+    for(int i=0; i<8; ++i){
+        As[TCSQ*i + ltid] = A[off + i*TCSQ + ltid];
+        Bs[TCSQ*i + ltid] = B[off + i*TCSQ + ltid];
+    }
+    // (2) sync
+    __syncthreads();
+
+    // solo el primer warp trabajara, de ocho warps
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a_frag;
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b_frag;
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, REAL> c_frag;
+
+    // (3) cargar datos de memoria global a A, B y C frags
+    wmma::load_matrix_sync(a_frag, As + woff, TCSIZE);
+    wmma::load_matrix_sync(b_frag, Bs + woff, TCSIZE);
+    wmma::fill_fragment(c_frag, 0.0f);
+
+    // (4) operacion matrix-multiply-accumulate (MMA)
+    wmma::mma_sync(c_frag, a_frag, b_frag, c_frag);
+
+    // (5) guardar datos de vuelta en memorial global
+    wmma::store_matrix_sync(C + off + woff, c_frag, TCSIZE, wmma::mem_row_major);
+}
+
+__global__ void matmuls_basic_half(half* A, half* B, REAL* C, int n){
+    int off = blockIdx.x * (TCSIZE*TCSIZE);
+    int tid = off + (threadIdx.y*TCSIZE + threadIdx.x);
+    half sum = 0.0f;
+    for(int i=0; i<TCSIZE; ++i){
+        sum += __hmul(A[off + threadIdx.y*TCSIZE + i],B[off + i*TCSIZE + threadIdx.x]);
+        //C[tid] += __hmul(A[off + threadIdx.y*TCSIZE + i],B[off + i*TCSIZE + threadIdx.x]);
+    }
+    C[tid] = (float)sum;
+}
+
+__global__ void matmuls_sm_half(half* A, half* B, REAL *C, int n){
+    __shared__ half As[TCSIZE*TCSIZE];
+    __shared__ half Bs[TCSIZE*TCSIZE];
+    __shared__ half Cs[TCSIZE*TCSIZE];
+    int off = blockIdx.x * (TCSIZE*TCSIZE);
+    int ltid = threadIdx.y*TCSIZE + threadIdx.x;
+    // (1) cargar datos de global mem a cache
+    As[ltid] = A[off + ltid];
+    Bs[ltid] = B[off + ltid];
+    Cs[ltid] = 0.0f;
+    // (2) sync
+    __syncthreads();
+    // (3) hacer multiplicacion usando As, Bs, Cs 
+    #pragma unroll
+    for(int i=0; i<TCSIZE; ++i){
+        Cs[ltid] += As[threadIdx.y*TCSIZE + i]*Bs[i*TCSIZE + threadIdx.x];
+    }
+    // (4) copiar Cs a memoria global.
+    C[off + ltid] = (float)Cs[ltid];
+}
+#endif
---
+++ matmuls/main.cu
@@ -0,0 +1,228 @@
+#include <cuda.h>
+#include <mma.h>
+#include <cstdio>
+#define REAL float
+#define TCSIZE 16
+#define TCSQ 256
+#define BSIZE 32
+#define PRINTLIMIT 10
+#include "kernel.cuh"
+
+
+#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
+inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort=true)
+{
+   if (code != cudaSuccess)
+   {
+      fprintf(stderr,"GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
+      if (abort) exit(code);
+   }
+}
+
+void last_cuda_error(const char *msg){
+	cudaError_t error = cudaGetLastError();
+	if(error != cudaSuccess){
+		// print the CUDA error message and exit
+		printf("[%s]: CUDA error: %s\n", msg, cudaGetErrorString(error));
+		exit(-1);
+	}
+}
+
+void initmat(REAL *m, int nmats, const int val){
+    for(int k=0; k<nmats; ++k){
+        int off = k*TCSIZE*TCSIZE;
+        for(int i=0; i<TCSIZE; ++i){
+            for(int j=0; j<TCSIZE; ++j){
+                m[off + i*TCSIZE + j] = (val*(k+1));
+            }
+        }
+    }
+}
+
+void printmats(REAL *m, int nmats, const char *msg){
+    printf("%s:\n", msg);
+    for(int k=0; k<nmats; ++k){
+        printf("k=%i\n", k);
+        int off = k*TCSIZE*TCSIZE;
+        for(int i=0; i<TCSIZE; ++i){
+            for(int j=0; j<TCSIZE; ++j){
+                printf("%.2f ", m[off + i*TCSIZE + j]);
+            }
+            printf("\n");
+        }
+    }
+}
+
+void timer_start(cudaEvent_t *start, cudaEvent_t *stop){
+    cudaEventRecord(*start);
+}
+
+void timer_stop(cudaEvent_t *start, cudaEvent_t *stop, const char *msg){
+    cudaEventRecord(*stop);
+    cudaEventSynchronize(*stop);
+    float time = 0.0f;
+    cudaEventElapsedTime(&time, *start, *stop);
+    printf("%s: %f secs\n", msg, time/1000.0f);
+}
+
+
+// main
+int main(int argc, char **argv){
+    // params
+    if(argc != 5){
+        fprintf(stderr, "run as ./prog dev nmats alg mode\n");
+        exit(EXIT_FAILURE);
+    }
+    int dev = atoi(argv[1]);
+    int nmats = atoi(argv[2]);
+    int alg = atoi(argv[3]);
+    int totaln = nmats*(TCSIZE)*(TCSIZE);
+    int mode = atoi(argv[4]);
+    printf("alg = %i  mode = %s nmats=%i  dev=%i   TCSIZE=%i  totaln=%i\n", alg, mode == 0? "FP16": "FP32", nmats, dev, TCSIZE, totaln);
+    
+    // set device
+    cudaSetDevice(dev);
+    cudaEvent_t start, stop;
+    cudaEventCreate(&start);
+    cudaEventCreate(&stop);
+
+    // mallocs
+    REAL *A,  *B,  *C;
+    REAL *Ad, *Bd, *Cd;
+    half *Adh, *Bdh, *Cdh;
+
+    printf("CPU mallocs (A[], B[], C[])......."); fflush(stdout);
+    timer_start(&start, &stop);
+    A = (REAL*)malloc(sizeof(REAL)*totaln);
+    B = (REAL*)malloc(sizeof(REAL)*totaln);
+    C = (REAL*)malloc(sizeof(REAL)*totaln);
+    timer_stop(&start, &stop, "done");
+
+    printf("GPU mallocs (A[], B[], C[])......."); fflush(stdout);
+    timer_start(&start, &stop);
+    cudaMalloc(&Ad, sizeof(REAL)*totaln);
+    cudaMalloc(&Bd, sizeof(REAL)*totaln);
+    cudaMalloc(&Cd, sizeof(REAL)*totaln);
+    cudaMalloc(&Adh, sizeof(half)*totaln);
+    cudaMalloc(&Bdh, sizeof(half)*totaln);
+    cudaMalloc(&Cdh, sizeof(half)*totaln);
+    timer_stop(&start, &stop, "done");
+
+    // init data 
+    printf("CPU init (A[], B[], C[]).........."); fflush(stdout);
+    timer_start(&start, &stop);
+    initmat(A, nmats, 1);
+    initmat(B, nmats, 1);
+    initmat(C, nmats, 0);
+    timer_stop(&start, &stop, "done");
+    
+
+    // copy data to GPU
+    printf("cudaMemcpy: Host -> Device........"); fflush(stdout);
+    timer_start(&start, &stop);
+    cudaMemcpy(Ad, A, sizeof(REAL)*totaln, cudaMemcpyHostToDevice);
+    cudaMemcpy(Bd, B, sizeof(REAL)*totaln, cudaMemcpyHostToDevice);
+    cudaMemcpy(Cd, C, sizeof(REAL)*totaln, cudaMemcpyHostToDevice);
+    timer_stop(&start, &stop, "done");
+
+    // convert to half
+    convertFp32ToFp16 <<< (totaln + 255)/256, 256 >>> (Adh, Ad, totaln);
+    convertFp32ToFp16 <<< (totaln + 255)/256, 256 >>> (Bdh, Bd, totaln);
+    convertFp32ToFp16 <<< (totaln + 255)/256, 256 >>> (Cdh, Cd, totaln);
+
+    // parallel structures
+    dim3 block, grid;
+
+    // kernel
+    if(alg == 0){
+        printf("\033[32;1m[matmul basico]\033[0m.............");
+        block = dim3(TCSIZE, TCSIZE, 1);    
+        grid = dim3((totaln+TCSQ-1)/TCSQ, 1, 1);    
+        timer_start(&start, &stop);
+        matmuls_basic<<<grid, block>>>(Ad, Bd, Cd, totaln);
+    }
+    else if(alg == 1){
+        printf("\033[32;1m[matmul sm]\033[0m.................");
+        block = dim3(TCSIZE, TCSIZE, 1);    
+        grid = dim3((totaln+TCSQ-1)/TCSQ, 1, 1);    
+        timer_start(&start, &stop);
+        matmuls_sm<<<grid, block>>>(Ad, Bd, Cd, totaln);
+    }
+    else if(alg == 2){
+        printf("\033[32;1m[matmul tc 16x2-blocks]\033[0m...........");
+        block = dim3(TCSIZE, 2, 1);    
+        //grid = dim3((totaln+TCSQ-1)/TCSQ, 1, 1);    
+        grid = dim3(nmats, 1, 1);    
+        timer_start(&start, &stop);
+        matmuls_tc<<<grid, block>>>(Adh, Bdh, Cd, totaln);
+    }
+    else if(alg == 3){
+        printf("\033[32;1m[matmul tc 16x16 blocks]\033[0m.......");
+        block = dim3(TCSIZE, TCSIZE, 1);    
+        grid = dim3((totaln+8*TCSQ-1)/(8*TCSQ), 1, 1);    
+        timer_start(&start, &stop);
+        matmuls_tc_block<<<grid, block>>>(Adh, Bdh, Cd, totaln);
+    }
+    else if(alg == 4){
+        printf("\033[32;1m[matmul tc 16x16 blocks-sm]\033[0m.......");
+        block = dim3(TCSIZE, TCSIZE, 1);    
+        grid = dim3((totaln+8*TCSQ-1)/(8*TCSQ), 1, 1);    
+        timer_start(&start, &stop);
+        matmuls_tc_block_sm<<<grid, block>>>(Adh, Bdh, Cd, totaln);
+    }
+    else if(alg == 5){
+        printf("\033[32;1m[matmul half]\033[0m..........");
+        block = dim3(TCSIZE, TCSIZE, 1);
+        grid = dim3((totaln+TCSQ-1)/TCSQ, 1, 1);
+        timer_start(&start, &stop);
+        matmuls_basic_half<<<grid, block>>>(Adh, Bdh, Cd, totaln);
+    }
+    else if(alg == 6){
+        printf("\033[32;1m[matmul half]\033[0m..........");
+        block = dim3(TCSIZE, TCSIZE, 1);
+        grid = dim3((totaln+TCSQ-1)/TCSQ, 1, 1);
+        timer_start(&start, &stop);
+        matmuls_sm_half<<<grid, block>>>(Adh, Bdh, Cd, totaln);
+    }
+    else{
+        printf("error: choose 0, 1, 2 for method\n");
+        exit(EXIT_FAILURE);
+    }
+    cudaDeviceSynchronize();
+    timer_stop(&start, &stop, "done");
+    printf("block(%i, %i, %i) grid(%i, %i, %i)\n", block.x, block.y, block.z, grid.x, grid.y, grid.z); fflush(stdout);
+
+
+    // copy GPU -> CPU
+    printf("cudaMemcpy: Device -> Host........"); fflush(stdout);
+    timer_start(&start, &stop);
+    cudaMemcpy(A, Ad, sizeof(REAL)*totaln, cudaMemcpyDeviceToHost);
+    cudaMemcpy(B, Bd, sizeof(REAL)*totaln, cudaMemcpyDeviceToHost);
+    cudaMemcpy(C, Cd, sizeof(REAL)*totaln, cudaMemcpyDeviceToHost);
+    timer_stop(&start, &stop, "done");
+
+    if(nmats < PRINTLIMIT){
+        printmats(A, nmats, "[after] mat A:");
+        printmats(B, nmats, "[after] mat B:");
+        printmats(C, nmats, "[after] mat C:");
+    }
+
+    // free memory in CPU and GPU
+    printf("host free (A, B, C)..............."); fflush(stdout);
+    timer_start(&start, &stop);
+    free(A);
+    free(B);
+    free(C);
+    timer_stop(&start, &stop, "done");
+    printf("device free (A, B, C)............."); fflush(stdout);
+    timer_start(&start, &stop);
+    cudaFree(Ad);
+    cudaFree(Bd);
+    cudaFree(Cd);
+    cudaFree(Adh);
+    cudaFree(Bdh);
+    timer_stop(&start, &stop, "done");
+
+    // timer results
+    exit(EXIT_SUCCESS);
+}
