9e0ac84b56fb6656e75c6c56e78ede158bb04764
Cristobal <cristobal.navarro.g@gmail.com>
2019-09-23 17:21:03 -0300

    fixed mixed reduction, now can compile with any R, mixed will always use R=1 internally

--- reduction/reduction-benchmark/src/kernel.cuh
+++ reduction/reduction-benchmark/src/kernel.cuh
@@ -98,6 +98,59 @@ __inline__ __device__ REAL reduction_tc_warp(int N, half *A, int offset, int lan
     else return 0.0f;
 }
 
+// kernel
+//__global__ void tc_reduction(half* A, int n){
+__inline__ __device__ REAL reduction_tc_warp_R1(int N, half *A, int offset, int lane, int warpoff){
+    // definicion de offset y fragmentos
+    wmma::fragment<wmma::matrix_a, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> a_frag;
+    wmma::fragment<wmma::matrix_b, WMMA_M, WMMA_N, WMMA_K, half, wmma::row_major> b_frag;
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, half> d_frag;
+    wmma::fragment<wmma::accumulator, WMMA_M, WMMA_N, WMMA_K, REAL> r_frag;
+    
+    // (1) cargar datos de memoria global a A, B y C frags
+    wmma::fill_fragment(a_frag, 1.0f);
+    //wmma::fill_fragment(b_frag, 0.0f);
+    wmma::fill_fragment(d_frag, 0.0f);
+
+    // (2) mejora MMA multiples encadenados
+    //const int bigoffset = gridDim.x * 32 * TCSQ;
+    //if(offset >= N){ return 0.0f; }
+    //if(threadIdx.x == 0) printf("offset %i \n",offset + TCSQ*32*(i+1));
+    wmma::load_matrix_sync(b_frag, A + offset, TCSIZE);
+    wmma::mma_sync(d_frag, a_frag, b_frag, d_frag);
+
+    // (3) preparando datos para segundo MMA
+    wmma::fill_fragment(b_frag, 1.0f);
+    // [OPCION 1] copia manual
+    //#pragma loop unroll
+    //for(int i=0; i < 8 ; ++i){
+    //    a_frag.x[i] = d_frag.x[i];
+    //    a_frag.x[i+8] = d_frag.x[i];
+    //}
+   
+    //int offwid = (threadIdx.x/32)*256;
+    // [OPCION 2] copia a shared mem
+    __shared__ half As[DIFF];
+    wmma::store_matrix_sync(As+warpoff, d_frag, TCSIZE, wmma::mem_row_major);
+    wmma::load_matrix_sync(a_frag, As+warpoff, TCSIZE);
+    wmma::fill_fragment(d_frag, 0.0f);
+
+
+
+
+    //// (4) MMA
+    wmma::mma_sync(r_frag, a_frag, b_frag, d_frag);
+
+    // (5) Almacenar resultado
+    if(lane == 0){
+        //printf("block: %i, val %f\n",blockIdx.x,(float)d_frag.x[0]);
+        //printf("%f\n",(float)d_frag.x[0]);
+        return r_frag.x[0];
+        //return 1.0f;
+    }
+    else return 0.0f;
+}
+
 __inline__ __device__ REAL block_reduce_tc(int N, half *a, int offset){
 	__shared__ REAL shared[WARPSIZE];
 	int tid = threadIdx.x;
@@ -161,13 +214,36 @@ __global__ void kernel_reduction_tc_blockshuffle(half *a, float *out, int N,int 
 
 
 
+//////////////////////
+// MIXED
+//////////////////////
+__inline__ __device__ REAL block_reduce_tc_R1(int N, half *a, int offset){
+	__shared__ REAL shared[WARPSIZE];
+	int tid = threadIdx.x;
+	int lane = tid & (WARPSIZE-1);
+	//int wid = tid/WARPSIZE;
+	int wid = tid >> 5;
+	REAL val = reduction_tc_warp_R1(N, a, offset + wid*TCSQ, lane, wid << 8);
+    //return val;
+	if(lane == 0){
+		shared[wid] = val;
+    }
+	__syncthreads();
+	//val = (tid < blockDim.x/WARPSIZE) ? shared[lane] : (REAL) 0.0f;
+    //printf("thread %i val %f\n", threadIdx.x, val);
+	val = (tid < (blockDim.x >> 5)) ? shared[lane] : 0.0f;
+	if(wid == 0){
+        val = warp_shuffle_reduction_real(val);
+    }
+	return val;
+}
 
-
+// [ TC MMAs ..... |  CLASSIC SHUFFLE ]
 __global__ void kernel_reduction_tc_mixed(int N, half *a, float *out, int bntc, int bns){
     REAL sum=0;
     int offset_tc = (blockIdx.x)*DIFF;
     if(blockIdx.x < bntc){
-        sum = block_reduce_tc(N, a, offset_tc);
+        sum = block_reduce_tc_R1(N, a, offset_tc);
     }
     else{
     //if(blockIdx.x >= bntc){
